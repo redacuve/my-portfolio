9ecb9b1a70afe5e261193ec2f6f8ce569df40f12
Rey David Cuevas <cuevas.vela@gmail.com>
2020-05-13 11:13:45 -0500

    Start the app project, and updated the layout

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {}
+    }
+
+    render(){
+        return(
+            <div>Primera Línea</div>
+        );
+    }
+
+}
+
+export default App;
