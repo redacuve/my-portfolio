7b8436d162a89e61e620f41eca2b8df5e03aacc2
Rey David Cuevas <cuevas.vela@gmail.com>
2020-05-15 18:45:44 -0500

    added form on contact page, changed to class instead a function

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -21,6 +21,16 @@ class Contact extends React.Component {
             emailSent: null
         }
     }
+
+    handleChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        
+        this.setState({
+            [name]: value
+        });
+    }
     
     render(){
         return(
@@ -40,15 +50,15 @@ class Contact extends React.Component {
                         <Form>
                             <Form.Group>
                                 <Form.Label htmlFor="full-name">Full Name:</Form.Label>
-                                <Form.Control id="full-name" name="name" type="text" value={this.state.name} />
+                                <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange} />
                             </Form.Group>
                             <Form.Group>
                                 <Form.Label htmlFor="email">Email:</Form.Label>
-                                <Form.Control id="email" name="email" type="email" value={this.state.email} />
+                                <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange} />
                             </Form.Group>
                             <Form.Group>
                                 <Form.Label htmlFor="message">Message:</Form.Label>
-                                <Form.Control id="message" name="message" as="textarea" rows="3" value={this.state.message} />
+                                <Form.Control id="message" name="message" as="textarea" rows="3" value={this.state.message} onChange={this.handleChange} />
                             </Form.Group>
                             <Form.Group>
                                 <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled} >Send</Button>
