27ee2c0406d90aa9eae250e6638c14c72f9e2bc2
Rey David Cuevas <cuevas.vela@gmail.com>
2020-05-15 11:58:10 -0500

    adde set list component

---
+++ src/components/SetList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function SetList(props){
+    return(
+        <div className="set set-list">
+            <h2>{props.title}</h2>
+            <ul className={(props.listflex === true) ? "d-flex" : ""}>
+                {
+                props.list.map(elem => {
+                    return <li key="elem">{elem}</li>
+                })
+                }
+            </ul>
+        </div>
+    );
+}
+
+export default SetList;
